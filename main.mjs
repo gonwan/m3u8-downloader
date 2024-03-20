fc4a53a494026c3f1d22b0cd0784d117a693d815
Binhao Qian <gonwan@gmail.com>
2024-03-21 03:14:03 +0800

    use electron 28 for esm support.

---
+++ main.mjs
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron/main'
+import path from 'node:path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.mjs')
+        }
+    });
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
