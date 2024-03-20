1a90414d8fcdabf93d25c5dd13febaae92e91851
Binhao Qian <gonwan@gmail.com>
2024-03-20 23:46:05 +0800

    initial commit.

---
+++ src/downloader.ts
@@ -0,0 +1,112 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import got, { Progress } from 'got';
+import { HttpProxyAgent } from 'http-proxy-agent';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+
+class DownloadOptions {
+
+    headers: object = {};
+    proxy: string = "";
+
+    concurrency: number = 1;
+    timeoutMs: number = 10*1000;
+    totalRetry: number = 10;
+
+    //preferredResolution: number = -1;
+    loggerCallback: object = {};
+
+}
+
+class DownloadManager {
+
+    options: DownloadOptions;
+
+    currentChunks: number = 0;
+    totalChunks: number = 0;
+    speedKB: number = 0;
+
+    constructor(options: DownloadOptions) {
+        this.options = options;
+    }
+
+    async downloadM3u8File(url: string) {
+        //console.log(this.options.headers);
+        return got.get(url,
+            {
+                headers: { 'User-Agent': 'Lavf/58.24.101' },
+                //headers: JSON.parse(JSON.stringify(this.options.headers)) /* object to record */
+                agent: {
+                    http: this.options.proxy ? new HttpProxyAgent(this.options.proxy) : undefined,
+                    https: this.options.proxy ? new HttpsProxyAgent(this.options.proxy) : undefined
+                }
+            })
+            .buffer();
+    }
+
+    async downloadOneSegment(dlUrl: string, ptPath: string, index: number, progressCallback?: (idx: number, progress: Progress) => void) {
+        let buff = await got.get(dlUrl,
+            {
+                headers: { 'User-Agent': 'Lavf/58.24.101' },
+                //headers: JSON.parse(JSON.stringify(this.options.headers)) /* object to record */
+                agent: {
+                    http: this.options.proxy ? new HttpProxyAgent(this.options.proxy) : undefined,
+                    https: this.options.proxy ? new HttpsProxyAgent(this.options.proxy) : undefined
+                }
+            })
+            .on('downloadProgress', progress => {
+                //console.log(`index: ${index}, progress: ${JSON.stringify(progress)}`);
+                if (progressCallback) {
+                    progressCallback(index, progress);
+                }
+            })
+            .buffer();
+        let outputPath = path.join(ptPath, `${index}.ts`);
+        await fs.promises.writeFile(outputPath, buff);
+    }
+
+    async downloadFirstSegment(dlUrl: string, ptPath: string) {
+        await this.downloadOneSegment(dlUrl, ptPath, 0);
+    }
+
+    async downloadSegments(dlUrls: string[], ptPaths: string[], indexes: number[]) {
+        let finishedSegs = 0;
+        let totalTransferredBytes = 0;
+        let requests = new Map<number, any>();
+        let transferredBytes = new Map<number, number>();
+        const pcb = (idx: number, progress: Progress) => {
+            transferredBytes.set(idx, progress.transferred);
+        }
+        let stats = setInterval(() => {
+            let prev = totalTransferredBytes;
+            totalTransferredBytes = 0;
+            transferredBytes.forEach((v) => {
+                totalTransferredBytes += v;
+            })
+            console.log(`download speed: ${(totalTransferredBytes-prev)/1000.0}kb/s, percent: ${finishedSegs/dlUrls.length*100}%.`);
+            }, 1000);
+        for (let i = 0; i < dlUrls.length; i++) {
+            if (requests.size <= this.options.concurrency) {
+                let p = this.downloadOneSegment(dlUrls[i], ptPaths[i], indexes[i], pcb).then(() => {
+                    requests.delete(indexes[i]);
+                    finishedSegs++;
+                })
+                requests.set(indexes[i], p);
+                //console.log('keys: ' + Array.from(requests.keys()));
+                if (requests.size == this.options.concurrency) {
+                    await Promise.any(requests.values());
+                }
+            }
+        }
+        await Promise.all(requests.values());
+        clearInterval(stats);
+    }
+
+}
+
+export { DownloadOptions, DownloadManager };
+
+// master.m3u8 --> playlists.json(no use..already selected), raw.m3u8 --> meta.json...
+// catch awaits...
+// index.m3u8 contains multiple sequences???
+// see: https://datatracker.ietf.org/doc/html/rfc8216, HTTP Live Streaming
---
+++ src/ffmpeg.ts
@@ -0,0 +1,103 @@
+import fs from 'node:fs';
+import process from 'node:process';
+import ffi from '@ffmpeg-installer/ffmpeg'
+import ffmpeg from 'fluent-ffmpeg'
+
+//ffmpeg.setFfmpegPath('C:\\Users\\gonwan\\Downloads\\N_m3u8DL-CLI_v3.0.2_with_ffmpeg_and_SimpleG\\ffmpeg.exe')
+ffmpeg.setFfmpegPath(ffi.path)
+console.log("version: " + ffi.version)
+
+const binaryConcat = async (files: string[], outputFile: string, baseDir: string) => {
+    console.log('files: ' + files + ', outputFile: ' + outputFile)
+    let cwd = process.cwd();
+    process.chdir(baseDir);
+    let ofile;
+    try {
+        ofile = await fs.promises.open(outputFile, 'w+');
+        for (let f of files) {
+            await ofile.appendFile(await fs.promises.readFile(f));
+        }
+    } catch (err) {
+        console.log('error: ' + err);
+    } finally {
+        await ofile?.close();
+        process.chdir(cwd);
+    }
+}
+
+/*
+ * see: https://trac.ffmpeg.org/wiki/Concatenate
+ * see: https://www.ffmpeg.org/ffmpeg.html#Stream-selection
+ */
+const ffmpegConcat = async (files: string[], outputFile: string, baseDir: string, format: string) => {
+    //console.log('files: ' + files + ', outputFile: ' + outputFile)
+    return new Promise((resolve, reject) => {
+        let protocol = 'concat:' + files.join('|');
+        let ff = ffmpeg(
+            {
+                logger: console,
+                cwd: baseDir
+            })
+            .input(protocol);
+        switch (format) {
+            case 'mp4':
+                ff = ff.addOptions(
+                    [
+                        '-map \"0:v?\"',
+                        '-map \"0:a?\"',
+                        '-map \"0:s?\"',
+                        '-c copy',
+                        '-bsf:a aac_adtstoasc',
+                        '-bsf:v h264_mp4toannexb'
+                    ])
+                    .output(`${outputFile}.mp4`)
+                break;
+            case 'mpegts':
+                ff = ff.addOptions(
+                    [
+                        '-map 0',
+                        '-c copy',
+                        '-copy_unknown',
+                        '-f mpegts',
+                    ])
+                    .output(`${outputFile}.ts`);
+                break;
+            default:
+                break;
+        }
+        ff
+            .on('start', (cmdline) => {
+                console.log(cmdline);
+            })
+            .on('end', () => {
+                console.log('merging finished!');
+                resolve(null);
+            })
+            .on('error', (err) => {
+                console.log('error: ' + err);
+                reject();
+            })
+            .run();
+    });
+}
+
+const ffmpegConcat2 = (file: string) => {
+    //let cwd = process.cwd();
+    let ff = ffmpeg({
+        logger: console,
+        cwd: ''
+    });
+    ff
+        .input(file)
+        .addOptions([
+            '-map 0',
+            '-c copy',
+            '-copy_unknown',
+            '-f mpegts',
+        ])
+        .on('start', (cmdline) => console.log(cmdline))
+        .output(file + '.mpeg')
+        .run();
+}
+
+export { binaryConcat, ffmpegConcat };
