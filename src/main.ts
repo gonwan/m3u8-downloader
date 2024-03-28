8bafc935b163961d5ef55ba953a341245a384d0c
Binhao Qian <gonwan@gmail.com>
2024-03-29 03:38:19 +0800

    on-demand import element plus.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
 import 'virtual:uno.css'
-import 'element-plus/dist/index.css';
 import './style.css';
 import App from './App.vue';
 import { router } from "./router";
 
 const app = createApp(App);
 
-app.use(ElementPlus);
 app.use(router)
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
 import electron from 'vite-plugin-electron/simple'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -48,6 +51,13 @@ export default defineConfig({
           }
         }
       }
+    }),
+    /* on-demand import, this lead to faster vue-tsc run and a much smaller bundle. */
+    AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()]
     })
   ]
 })
