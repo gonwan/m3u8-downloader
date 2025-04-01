c7c04afeb1b7da4391c504bdcb402ded125c9812
Binhao Qian <gonwan@gmail.com>
2025-04-01 19:34:07 +0800

    add ffmpeg path selection.

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -14,6 +14,11 @@ const selectFFmpegExePath = async () => {
   }
 }
 
+const resetFFmpegExePath = async () => {
+  form.ffmpegExePath = '';
+  localStorage.removeItem('md.ffmpegExePath');
+}
+
 const openLogFolder = async () => {
    await window.$electron.openLogFolder();
 }
@@ -31,11 +36,14 @@ const openLogFolder = async () => {
   <el-form label-width="auto" max-w-800>
   <h4 text-left mb-4>FFmpeg</h4>
   <el-row>
-    <el-col :span="22">
-      <el-input v-model.trim="form.ffmpegExePath" placeholder="FFmpeg executable path" clearable />
+    <el-col :span="20">
+      <el-input v-model.trim="form.ffmpegExePath" placeholder="Customize FFmpeg executable path, or reset to use default..." clearable />
     </el-col>
     <el-col :span="2">
       <el-button plain w-full @click="selectFFmpegExePath">Select</el-button>
+    </el-col>
+    <el-col :span="2">
+      <el-button plain w-full @click="resetFFmpegExePath">Reset</el-button>
     </el-col>
   </el-row>
   <h4 text-left mb-4>Debugging</h4>
